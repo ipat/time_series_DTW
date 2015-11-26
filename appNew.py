19cebd1c3c6659aaa58a563ce9d5fd42b627514e
Pat Hornchaiya <pat.hornchaiya@hotmail.com>
2015-11-26 17:41:49 +0700

    Add new type of neighbour

--- appNew.py
+++ appNew.py
@@ -3,19 +3,22 @@ import matplotlib.pyplot as plt
 import math
 
 def DTW(train_array, test_array, w_a, w_b, w_c):
-	distance_matrix = np.empty((len(train_array)+1, len(test_array)+1, ))
+	distance_matrix = np.empty((len(train_array)+2, len(test_array)+2, ))
 
 	distance_matrix[0, :] = 9999999
 	distance_matrix[:, 0] = 9999999
+	distance_matrix[1, :] = 9999999
+	distance_matrix[:, 1] = 9999999
 
-	distance_matrix[0,0] = 0
+	# distance_matrix[0,0] = 0
+	distance_matrix[1,1] = 0
 
-	for i in range(1, len(train_array) + 1):
-		for j in range(1, len(train_array) + 1):
-			cost = math.fabs(train_array[i - 1] - test_array[j - 1])
-			a = (cost * w_a) + distance_matrix[i-1, j]
-			b = (cost * w_b) + distance_matrix[i, j-1]
-			c = (cost * w_c) + distance_matrix[i-1,j-1]
+	for i in range(2, len(train_array) + 2):
+		for j in range(2, len(train_array) + 2):
+			# cost = math.fabs(train_array[i - 1] - test_array[j - 1])
+			a = math.fabs(train_array[i - 4] - test_array[j - 2]) + distance_matrix[i-2, j]
+			b = math.fabs(train_array[i - 2] - test_array[j - 2]) + distance_matrix[i, j-2]
+			c = math.fabs(train_array[i - 2] - test_array[j - 4]) + distance_matrix[i-1,j-1]
 			if a > b:
 				if b > c:
 					min_val = c
@@ -67,14 +70,14 @@ for w_a in range(1, 4):
 						min_dist = dist
 						answer = current_train_answer
 
-				# print "Trained Answer : " + str(answer)
-				# print "Real Answer: " + str(current_test_answer)
-				# print "------------------------------------"
+				print "Trained Answer : " + str(answer)
+				print "Real Answer: " + str(current_test_answer)
+				print "------------------------------------"
 
 				if int(answer) == int(current_test_answer):
 					accuracy += 1
-				# print "Current Accuracy : " + str(accuracy) + "/" + str(len(test_answer))
-				# print "===================================="
+				print "Current Accuracy : " + str(accuracy) + "/" + str(len(test_answer))
+				print "===================================="
 			
 			print "Weight(a,b,c): " + str(w_a) + ", " + str(w_b) + ", " + str(w_c)
 			print "Accuracy : " + str(accuracy) + "/" + str(len(test_answer))
